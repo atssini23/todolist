a6bc99d78682bc4121d0a188a4b1383078699cfb
Atssini Cabrera <atssinicabrera@Atssinis-MacBook-Pro.local>
2018-03-14 10:03:25 -0700

    mysql connection

--- todolist/settings.py
+++ todolist/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'todolist.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql', 
+        'NAME': 'Todoslist',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': '127.0.0.1',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
     }
 }
 
